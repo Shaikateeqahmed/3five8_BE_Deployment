be12e4c588c263285b0aeb0504680d78f8f99118
Shaikateeqahmed <shaikateeqahmed892@gmail.com>
2023-08-02 13:52:28 +0530

    For deployment

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ require("dotenv").config();
 
 const app = express();
 
+app.get("/",(req,res)=>{
+    res.send("Home Page");
+})
+
 app.use(express.json());
 app.use(cors());
 app.use("/user",User);
